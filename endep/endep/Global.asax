d619a10c29af6077e1c9fc35db4e1aa432078a2e
darwinvasquez <jhondarwinvasq@gmail.com>
2017-04-17 18:55:31 -0500

    Inicio plantilla

---
+++ endep/endep/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="endep.MvcApplication" Language="C#" %>
---
+++ endep/endep/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(endep.Startup))]
+namespace endep
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
